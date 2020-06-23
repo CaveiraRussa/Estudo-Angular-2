ac265468c40ed884678d1bc2479402c60f1ae6f7
Luiz Augusto <luiz.m.santiago@hotmail.com>
2020-06-23 17:07:52 -0300

    Criado modulo de fotos

--- alurapic/src/app/app.component.ts
+++ alurapic/src/app/app.component.ts
@@ -1,20 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PhotoService } from './photos/photo/photo.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  photos = [
-    {
-      url: 'https://ovicio.com.br/wp-content/uploads/2019/06/20190609-cyberpunk-02-1200x675.jpg',
-      description: 'Keano'
-    },
-    {
-      url: 'https://i.pinimg.com/originals/a9/a2/79/a9a279ae61cd00eb2b3a4978a62a5ad5.jpg',
-      description: 'Pai'
-    }
-  ];
+export class AppComponent implements OnInit {
+  photos: Object[] = [];
+  
+  constructor(private photoService: PhotoService) { }
+
+  ngOnInit(): void {
+
+      this.photoService
+          .listFromUser('flavio')
+          .subscribe(photos => this.photos = photos);
+    } 
 }
  
